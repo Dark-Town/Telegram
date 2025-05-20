029228d273e9c3b7f034063840c7b8a12f9936d4
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-20 13:46:08 +0200

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.environ.get("7653249811:AAEk7AzVb4Rcl-8Wb5ZLYqA8SfbD1W7pmqs")
+API_ID = int(os.environ.get("24413166"))
+API_HASH = os.environ.get("7d43dfedf3baa98292adb7b3f69cd6bd")
+
+# If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
+
+# BOT_TOKEN = "<your bottoken>"
+# API_ID = "<your api_id>"
+# API_HASH = "<your api_hash>"
