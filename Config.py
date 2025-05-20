edac62305408fa2051339bbe59779a8cfc7181ad
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-05-20 14:51:41 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = 7653249811:AAEk7AzVb4Rcl-8Wb5ZLYqA8SfbD1W7pmqs
-API_ID = 24413166
-API_HASH = 7d43dfedf3baa98292adb7b3f69cd6bd
+BOT_TOKEN = "7653249811:AAEk7AzVb4Rcl-8Wb5ZLYqA8SfbD1W7pmqs"
+API_ID = "24413166"
+API_HASH = "7d43dfedf3baa98292adb7b3f69cd6bd"
 
 # If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
 
